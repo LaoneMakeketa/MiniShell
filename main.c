823a176953a344990fb3e9b7ef466a98401083cc
Laone <laonemakeketa@gmail.com>
2026-09-04 12:04:07 +0200

    Added a utility function for safe memory allocation

--- main.c
+++ main.c
@@ -1,4 +1,15 @@
-#include <stdio.h>
+ #include <stdio.h>
+ #include <stdlib.h>
+
+void *safe_malloc(size_t size){
+    void* ptr = malloc(size);
+    if (ptr == NULL){
+        printf("Memory allocation failed.");
+        exit(1);
+    }else{
+        return ptr;
+    }
+}
 
 int main(){
     printf("Shell starting...");
